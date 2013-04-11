a14d147b5751acccdb2d76426447dcbc7faa9512
Cees de Jonge <c.de.jonge@philips.com>
2013-04-11 15:39:52 +0300

    Update transmartApp-DB_Post.sql
    Added a line to set "heatmap" defenition in "searchapp.plugin_module" right?

--- Postgres/GPL 1.0/TM_CZ/transmartApp-DB_Post.sql
+++ Postgres/GPL 1.0/TM_CZ/transmartApp-DB_Post.sql
@@ -49,5 +49,14 @@ BEGIN
     EXCEPTION
         WHEN duplicate_column THEN RAISE NOTICE 'column "data_type" already exists in table "tm_wz.wt_trial_nodes"';
     END;
+
+  -- in Table "searchapp.plugin_module" the "heatmap" definition doesnot seems to be right
+  -- Also some definitions in this table seems to be missing.
+    BEGIN
+        update searchapp.plugin_module set params = '{"id":"heatmap","converter":{"R":["source(''||PLUGINSCRIPTDIRECTORY||Common/dataBuilders.R'')","source(''||PLUGINSCRIPTDIRECTORY||Common/ExtractConcepts.R'')","source(''||PLUGINSCRIPTDIRECTORY||Common/collapsingData.R'')","source(''||PLUGINSCRIPTDIRECTORY||Common/parseDirectory.R'')","source(''||PLUGINSCRIPTDIRECTORY||Heatmap/BuildHeatmapData.R'')","\t\t\t\t\t\tparseDirectory(topLevelDirectory = ''||TOPLEVELDIRECTORY||'')\n\t\t\t\t\t","\t\t\t\t\t\t\tHeatmapData.build(input.gexFile = ''||TOPLEVELDIRECTORY||/workingDirectory/mRNA.trans'',\n\t\t\t\t\t\t\tgenes = ''||GENES||'',\n\t\t\t\t\t\t\tgenes.aggregate = ''||AGGREGATE||'',\n\t\t\t\t\t\t\tsample.subset1=''||SAMPLE1||'',\n\t\t\t\t\t\t\ttime.subset1=''||TIMEPOINTS1||'',\n\t\t\t\t\t\t\ttissues.subset1=''||TISSUES1||'',\n\t\t\t\t\t\t\tplatform.subset1=''||GPL1||'',\n\t\t\t\t\t\t\tsample.subset2=''||SAMPLE2||'',\n\t\t\t\t\t\t\ttime.subset2=''||TIMEPOINTS2||'',\n\t\t\t\t\t\t\ttissues.subset2=''||TISSUES2||'',\n\t\t\t\t\t\t\tplatform.subset2=''||GPL2||'')\n\t\t\t\t\t"]},"name":"Heatmap","dataFileInputMapping":{"CLINICAL.TXT":"FALSE","SNP.TXT":"snpData","MRNA_DETAILED.TXT":"TRUE"},"dataTypes":{"subset1":["CLINICAL.TXT"]},"pivotData":false,"view":"Heatmap","processor":{"R":["source(''||PLUGINSCRIPTDIRECTORY||Heatmap/HeatmapLoader.R'')","\t\t\t\t\tHeatmap.loader(\n\t\t\t\t\tinput.filename=''outputfile''\n\t\t\t\t\t)\n\t\t\t\t\t"]},"renderer":{"GSP":"/RHeatmap/heatmapOut"},"variableMapping":{"||GENES||":"divIndependentVariablePathway","||AGGREGATE||":"divIndependentVariableprobesAggregation","||TIMEPOINTS1||":"divIndependentVariabletimepoints","||TISSUES1||":"divIndependentVariabletissues","||SAMPLE1||":"divIndependentVariablesamples","||GPL1||":"divIndependentVariablegplsValue","||TIMEPOINTS2||":"divIndependentVariabletimepoints2","||TISSUES2||":"divIndependentVariabletissues2","||SAMPLE2||":"divIndependentVariablesamples2","||GPL2||":"divIndependentVariablegplsValue2"}}' where module_seq =33;
+    EXCEPTION
+        WHEN others THEN RAISE NOTICE 'Problems updating table "searchapp.plugin_module" (column "params" for heatmap)';
+    END;
+
       
 END $$
