0e91c0e8077b79f0d49dbf99c06e1bd0a2e981fb
Jan Kanis <jan.code@jankanis.nl>
2013-12-09 12:27:17 +0100

    Split ETL load scripts into several lines so they are actually readable

--- Kettle-GPL/scripts/RBM_ANNOTATION_ETL.sh
+++ Kettle-GPL/scripts/RBM_ANNOTATION_ETL.sh
@@ -1,2 +1,10 @@
 #! /bin/bash
-./data-integration/kitchen.sh -norep=Y -file=Kettle-ETL/Req5_RBM_ANNOTATION/load_rbm_annotation.kjb -log=load_RBM_ANNOTATION_data.log -param:DATA_LOCATION=exam -param:SORT_DIR=/tmp -param:GPL_ID=RBM100 -param:LOAD_TYPE=I -param:ANNOTATION_TITLE='RBM'
+./data-integration/kitchen.sh \
+-norep=Y \
+-file=Kettle-ETL/Req5_RBM_ANNOTATION/load_rbm_annotation.kjb \
+-log=load_RBM_ANNOTATION_data.log \
+-param:DATA_LOCATION=exam \
+-param:SORT_DIR=/tmp \
+-param:GPL_ID=RBM100 \
+-param:LOAD_TYPE=I \
+-param:ANNOTATION_TITLE='RBM'
--- Kettle-GPL/scripts/RNA_Sequencing_Annotation_ETL.sh
+++ Kettle-GPL/scripts/RNA_Sequencing_Annotation_ETL.sh
@@ -1,2 +1,8 @@
 #! /bin/bash
-./data-integration/kitchen.sh -norep=Y -file=Kettle-ETL/Req11_RNASEQ_ANNOTATION/load_rnaseq_annotation.kjb -log=load_RNA_sequencing_annotation.log -param:DATA_LOCATION=exam -param:SORT_DIR=/tmp -param:DATA_FILE=rna_seq_annotation_sample.txt 
+./data-integration/kitchen.sh \
+-norep=Y \
+-file=Kettle-ETL/Req11_RNASEQ_ANNOTATION/load_rnaseq_annotation.kjb \
+-log=load_RNA_sequencing_annotation.log \
+-param:DATA_LOCATION=exam \
+-param:SORT_DIR=/tmp \
+-param:DATA_FILE=rna_seq_annotation_sample.txt 
