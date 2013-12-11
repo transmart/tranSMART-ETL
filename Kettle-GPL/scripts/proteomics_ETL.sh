32534cd6dd394eba4b9a289a7fe31c89ae448e8f
Ruslan Forostianov <ruslan@thehyve.nl>
2013-12-11 11:13:29 +0100

    Proteomics support
    - ETL
    - Sample data
    - DB changes

---
+++ Kettle-GPL/scripts/proteomics_ETL.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+./data-integration/kitchen.sh -norep=Y -file=Kettle-ETL/REQ8_PROTEOMICS_PIVOT_UPDATED/load_proteomics_data.kjb -log=load_proteomics_pivot_data.log -param:DATA_LOCATION=exam -param:STUDY_ID=GSE37425 -param:MAP_FILENAME=proteomics_sub_sam_data.txt -param:DATA_TYPE='R' -param:SORT_DIR=/tmp -param:TOP_NODE='\Public Studies\ExampleStudy\protiens' -param:LOAD_TYPE=I -param:DATA_FILE_PREFIX=proteomics_sample
