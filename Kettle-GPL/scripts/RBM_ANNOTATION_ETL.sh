1d6327b42ca8386157f7894c8046e1cd272207ce
Ruslan Forostianov <ruslan@thehyve.nl>
2013-12-20 10:59:40 +0100

    RBM ETL changes. Added new RBM sample files

--- Kettle-GPL/scripts/RBM_ANNOTATION_ETL.sh
+++ Kettle-GPL/scripts/RBM_ANNOTATION_ETL.sh
@@ -5,6 +5,6 @@
 -log=load_RBM_ANNOTATION_data.log \
 -param:DATA_LOCATION=../data/rbm-annotation \
 -param:SORT_DIR=/tmp \
--param:GPL_ID=RBM100 \
+-param:GPL_ID=RBM \
 -param:LOAD_TYPE=I \
 -param:ANNOTATION_TITLE='RBM'
