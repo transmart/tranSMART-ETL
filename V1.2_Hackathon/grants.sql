8ba50d58bfc7cfa4a73b4579b22e454231459cf5
Wu, Haiguo (US - Newton) <haiwu@deloitte.com>
2014-04-11 15:40:49 -0400

    add to V1.2 Hackathon  (February 2014)

---
+++ V1.2_Hackathon/grants.sql
@@ -0,0 +1,16 @@
+grant all on all tables in schema biomart to tm_cz;
+grant all on all tables in schema biomart_stage to tm_cz;
+grant all on all tables in schema deapp to tm_cz;
+grant all on all tables in schema i2b2demodata to tm_cz;
+grant all on all tables in schema i2b2metadata to tm_cz;
+grant all on all tables in schema searchapp to tm_cz;
+grant all on all tables in schema tm_lz to tm_cz;
+grant all on all tables in schema tm_wz to tm_cz;
+
+grant all on all sequences in schema biomart to tm_cz;
+grant all on all sequences in schema deapp to tm_cz;
+grant all on all sequences in schema i2b2demodata to tm_cz;
+grant all on all sequences in schema i2b2metadata to tm_cz;
+grant all on all sequences in schema searchapp to tm_cz;
+grant all on all sequences in schema tm_lz to tm_cz;
+grant all on all sequences in schema tm_wz to tm_cz;
