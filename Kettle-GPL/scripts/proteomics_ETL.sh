f74218479aaf22921129e05d10eec882cc91883d
forus <foro.ru@gmail.com>
2014-05-14 04:55:29 +0200

    Update proteomics_ETL.sh
    This shell script is just example and needs to be modified with own parameters anyway.

--- Kettle-GPL/scripts/proteomics_ETL.sh
+++ Kettle-GPL/scripts/proteomics_ETL.sh
@@ -1,2 +1,14 @@
 #! /bin/bash
-./data-integration/kitchen.sh -norep=Y -file=/Users/timdo/Projects/proteomics-CLUC-data/Kettle-ETL_proteomics/load_proteomics_data.kjb -log=load_proteomics_pivot_data.log -param:DATA_LOCATION=/Users/timdo/Projects/proteomics-CLUC-data/Kettle-ETL_proteomics/example_data -param:CHECK=hello -param:STUDY_ID=GSE8581 -param:MAP_FILENAME=proteomics_sub_sam_data.txt -param:DATA_TYPE='R' -param:SORT_DIR=/tmp -param:TOP_NODE='\Public Studies\GSE8581' -param:COLUMN_MAPPING_FILE=proteomics_column_mapping.txt -param:LOAD_TYPE=I -param:DATA_FILE_PREFIX=pp-Peptide-report_prepared
+./data-integration/kitchen.sh \
+-norep=Y \
+-file=/load_proteomics_data.kjb \
+-log=load_proteomics_pivot_data.log \
+-param:DATA_LOCATION=../data/proteomics \
+-param:STUDY_ID=GSE8581 \
+-param:MAP_FILENAME=proteomics_sub_sam_data.txt \
+-param:DATA_TYPE='R' \
+-param:SORT_DIR=/tmp \
+-param:TOP_NODE='\Public Studies\GSE8581' \
+-param:COLUMN_MAPPING_FILE=proteomics_column_mapping.txt \
+-param:LOAD_TYPE=I \
+-param:DATA_FILE_PREFIX=pp-Peptide-report_prepared
